dda666892e04826cff5620418671f6d4c993bfda
mlssymnkst <nakasatoyumi.it@gmail.com>
2026-05-07 19:28:35 -0300

    Corrigindo a virgula no create table

--- login/login.sql
+++ login/login.sql
@@ -7,12 +7,12 @@ DROP TABLE IF EXISTS entrada CASCADE;
 CREATE TABLE entrada (
 cod_usuario SERIAL PRIMARY KEY,
 usuario VARCHAR(20) NOT NULL,
-senha VARCHAR(10) NOT NULL,
+senha VARCHAR(10) NOT NULL
 );
 
 -- POPULANDO entrada
 SELECT * FROM entrada;
-INSERT INTO entrada VALUES (default, 'haruka', 'Liz06122025'); 
+INSERT INTO entrada VALUES (default, 'haruka', 'Liz061225'); 
 INSERT INTO entrada VALUES (default, 'marly', 'Tapi1112');
 INSERT INTO entrada VALUES (default, 'admin', 'admin01');
 INSERT INTO entrada VALUES (default, 'luiz', '12345');
