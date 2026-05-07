b62824ab2392f25f2866b339a45d64b4886a25e8
mlssymnkst <nakasatoyumi.it@gmail.com>
2026-05-07 18:53:09 -0300

    Removendo duplicatas do login

--- login/login.sql
+++ login/login.sql
@@ -1,20 +1,3 @@
-
--- Configuração de Data BR
-SET DATESTYLE TO POSTGRES, DMY ;
-SELECT current_date, current_timestamp ;
-
--- TABELA: entrada
-DROP TABLE IF EXISTS entrada CASCADE;
-CREATE TABLE entrada (
-cod_usuario SERIAL PRIMARY KEY,
-usuario VARCHAR(20) NOT NULL,
-senha VARCHAR(10) NOT NULL,
-);
-
--- POPULANDO entrada
-SELECT * FROM entrada;
-INSERT INTO entrada VALUES (default, 'haruka', '1008Liz'); 
-INSERT INTO entrada VALUES (default, 'marly', 'Tapi1225');
 -- Configuração de Data BR
 SET DATESTYLE TO POSTGRES, DMY ;
 SELECT current_date, current_timestamp ;
